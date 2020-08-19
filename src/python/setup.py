c3bcf9b4c9c04c35d53c798d33d2555b82c3071c
Holger Priwitzer <holger.priwitzer@eas.iis.fraunhofer.de>
2020-08-20 00:35:01 +0200

    bugfix: missing comma

--- src/python/setup.py
+++ src/python/setup.py
@@ -5,7 +5,8 @@ setup(name='mypkg',
       description='The funniest joke in the world',
       url='"https://github.com/dummy74/test-repo"',
       author='Dummy',
-      author_email='dummy@dummy.com'
+      author_email='dummy@dummy.com',
       license='MIT',
       packages=['mypkg'],
-      zip_safe=False)
+      zip_safe=False,
+)
