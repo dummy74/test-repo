2dc31b551afb907ebb723887c56cda3ae71b7797
Holger Priwitzer <holger.priwitzer@eas.iis.fraunhofer.de>
2020-08-20 00:15:42 +0200

    initial

---
+++ src/python/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='mypkg',
+      version='0.1',
+      description='The funniest joke in the world',
+      url='"https://github.com/dummy74/test-repo"',
+      author='Dummy',
+      author_email='dummy@dummy.com'
+      license='MIT',
+      packages=['mypkg'],
+      zip_safe=False)
